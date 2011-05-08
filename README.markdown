6c86f097d8228ce30e8e44be93fc360da1cf4825
Jeff Escalante <hello@jenius.me>
2011-05-08 16:54:40 -0400

    Fixed strange error

---
+++ README.markdown
@@ -0,0 +1,3 @@
+# This is a really great app
+
+Do I need to say anything more? With this guidance of [*this ruby on rails tutorial*](http://railstutorial.org/), I will make magic happen with barely any effort. You knoww!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '4f4ec4a397b17ff3a34406b12a16ff6a1962affd8af9eb3314865d9076f3ff45b64bce1e4af5b179fdcf255359794a6088e640a905a6dcca3e3095586bc73d8f'
